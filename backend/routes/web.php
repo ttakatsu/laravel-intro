f9af8876d02b7a9d791eddd1a63e4f04adb2d06d
ttakatsu <ttakatsu@gmail.com>
2021-01-23 16:47:31 +0900

    artisanで Breeze をインストール $ php artisan breeze:install

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/phpinfo', function() {
-    return phpinfo();
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
