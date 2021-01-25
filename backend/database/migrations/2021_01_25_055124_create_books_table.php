1c7da203693952c5a138f569191d0e72ae73479b
ttakatsu <ttakatsu@gmail.com>
2021-01-25 15:32:21 +0900

    Book テーブル作成のマイグレーションを作成

---
+++ backend/database/migrations/2021_01_25_055124_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Book テーブル作成のマイグレーション
+ */
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->integer('price');
+            $table->string('author', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
